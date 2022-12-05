92a6d26aff5bd29e4b82f36c02f1f6ffa98ae9ea
Le Dinh Quyen <quyenld9699@gmail.com>
2022-12-05 18:03:44 +0700

    update ui

--- src/views/Projects/DetailProject/DetailNode/Delegate/Delegate.tsx
+++ src/views/Projects/DetailProject/DetailNode/Delegate/Delegate.tsx
@@ -6,11 +6,11 @@ import { TableRow } from 'src/common/Table/TableRow';
 
 function LineData({ title, value }: { title: string; value: string }) {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', maxWidth: '400px' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <Typography variant="body1" color="text.secondary">
                 {title}
             </Typography>
-            <Typography variant="body1" color="text.primary">
+            <Typography variant="body1" color="text.primary" sx={{ fontWeight: 500 }}>
                 {value}
             </Typography>
         </Box>
@@ -19,104 +19,118 @@ function LineData({ title, value }: { title: string; value: string }) {
 
 export default function Delegate() {
     return (
-        <>
-            <BoxWrapper sx={{ bgcolor: 'background.paper', mt: 3, boxShadow: 3 }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="Validator:" value="Node 1" />
-                    </Grid>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="Chain ID:" value="Oraichain mainnet" />
-                    </Grid>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="Voting Power:" value="5%(1231token)" />
-                    </Grid>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="Self bonded:" value="1000 token" />
-                    </Grid>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="Address:" value="sdasdf...dfdff" />
-                    </Grid>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="Uptime:" value="100%" />
-                    </Grid>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="Commission:" value="1%" />
-                    </Grid>
-                    <Grid item xs={12} xsm={6}>
-                        <LineData title="APR:" value="53.67%" />
-                    </Grid>
+        <Box>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <BoxWrapper sx={{ bgcolor: 'background.paper', mt: 3, boxShadow: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <LineData title="Validator:" value="Node 1" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LineData title="Chain ID:" value="Oraichain mainnet" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LineData title="Voting Power:" value="5%(1231token)" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LineData title="Self bonded:" value="1000 token" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LineData title="Address:" value="sdasdf...dfdff" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LineData title="Uptime:" value="100%" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LineData title="Commission:" value="1%" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LineData title="APR:" value="53.67%" />
+                            </Grid>
+                        </Grid>
+                    </BoxWrapper>
                 </Grid>
-            </BoxWrapper>
-            <BoxWrapper sx={{ bgcolor: 'background.paper', mt: 3, boxShadow: 3 }}>
-                <Alert severity="success" color="info">
-                    <AlertTitle>Your delegated value:</AlertTitle>
-                    <LineData title="12 ORAI" value="~$1000"></LineData>
-                </Alert>
-                <Box sx={{ mt: 3 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={5} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                            <FormControl fullWidth variant="outlined">
-                                <InputLabel htmlFor="input-stake-more">Stake more token</InputLabel>
-                                <OutlinedInput
-                                    id="input-stake-more"
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <Button variant="outlined">Max</Button>
-                                        </InputAdornment>
-                                    }
-                                    label="Stake more token"
-                                />
-                            </FormControl>
-                            <Button variant="contained" fullWidth sx={{ mt: 2 }}>
-                                Delegate
-                            </Button>
-                        </Grid>
-                        <Grid item xs={12} md={7}>
-                            <Table sx={{ bgcolor: 'background.default' }}>
-                                <TableHeader spacing={1}>
-                                    <Grid item xs={4}>
-                                        Daily earning
-                                    </Grid>
-                                    <Grid item xs={4}>
-                                        Monthly earning
-                                    </Grid>
-                                    <Grid item xs={4}>
-                                        Yearly earning
-                                    </Grid>
-                                </TableHeader>
-                                <Divider sx={{ mt: 1, mb: 2 }} />
-                                <TableRow spacing={1}>
-                                    <Grid item xs={4}>
+                <Grid item xs={12} sm={6}>
+                    <BoxWrapper sx={{ bgcolor: 'background.paper', mt: 3, boxShadow: 3 }}>
+                        <Alert severity="success" color="info">
+                            <AlertTitle>
+                                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <Typography>Your delegated value:</Typography>
+                                    <Box>
+                                        <Typography sx={{ fontWeight: 500 }}>12 ORAI</Typography>
+                                        <Typography>~$1000</Typography>
+                                    </Box>
+                                </Box>
+                            </AlertTitle>
+                        </Alert>
+                        <Box sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                                    <FormControl fullWidth variant="outlined">
+                                        <InputLabel htmlFor="input-stake-more">Stake more token</InputLabel>
+                                        <OutlinedInput
+                                            id="input-stake-more"
+                                            endAdornment={
+                                                <InputAdornment position="end">
+                                                    <Button variant="outlined">Max</Button>
+                                                </InputAdornment>
+                                            }
+                                            label="Stake more token"
+                                        />
+                                    </FormControl>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                        <Typography variant="body1" color="text.secondary">
+                                            Daily earning
+                                        </Typography>
                                         <Box>
-                                            <Typography variant="body1">0 ORAI</Typography>
-                                            <Typography variant="body3" color={'text.secondary'}>
+                                            <Typography variant="body1" color="text.primary" sx={{ fontWeight: 500, textAlign: 'right' }}>
+                                                0 ORAI
+                                            </Typography>
+                                            <Typography variant="body1" color="text.primary" sx={{ textAlign: 'right' }}>
                                                 ~ $0
                                             </Typography>
                                         </Box>
-                                    </Grid>
-                                    <Grid item xs={4}>
+                                    </Box>
+                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 1 }}>
+                                        <Typography variant="body1" color="text.secondary">
+                                            Monthly earning
+                                        </Typography>
                                         <Box>
-                                            <Typography variant="body1">0 ORAI</Typography>
-                                            <Typography variant="body3" color={'text.secondary'}>
+                                            <Typography variant="body1" color="text.primary" sx={{ fontWeight: 500, textAlign: 'right' }}>
+                                                0 ORAI
+                                            </Typography>
+                                            <Typography variant="body1" color="text.primary" sx={{ textAlign: 'right' }}>
                                                 ~ $0
                                             </Typography>
                                         </Box>
-                                    </Grid>
-                                    <Grid item xs={4}>
+                                    </Box>
+                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 1 }}>
+                                        <Typography variant="body1" color="text.secondary">
+                                            Yearly earning
+                                        </Typography>
                                         <Box>
-                                            <Typography variant="body1">0 ORAI</Typography>
-                                            <Typography variant="body3" color={'text.secondary'}>
+                                            <Typography variant="body1" color="text.primary" sx={{ fontWeight: 500, textAlign: 'right' }}>
+                                                0 ORAI
+                                            </Typography>
+                                            <Typography variant="body1" color="text.primary" sx={{ textAlign: 'right' }}>
                                                 ~ $0
                                             </Typography>
                                         </Box>
-                                    </Grid>
-                                </TableRow>
-                            </Table>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </BoxWrapper>
-        </>
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button variant="contained" fullWidth>
+                                        Delegate
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </BoxWrapper>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
